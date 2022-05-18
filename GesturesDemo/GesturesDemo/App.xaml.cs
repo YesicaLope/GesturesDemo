ec5f1ca38ff057558a5022f0e9f6693d43e1e9eb
YesicaLope <yesica.lope@tecsup.edu.pe>
2022-05-18 17:41:55 -0500

    Agregar archivos de proyecto.

---
+++ GesturesDemo/GesturesDemo/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using GesturesDemo.Views;
+using Xamarin.Forms.Xaml;
+
+namespace GesturesDemo
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+        public static float ScreenWidth { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+           // DependencyService.Register<MockDataStore>();
+            MainPage = new SwipeDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
